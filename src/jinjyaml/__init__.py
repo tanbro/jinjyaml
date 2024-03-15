5717f8ec64735fbbde2c68f0a765a39bacfcc5e4
tanbro <tanbro@163.com>
2024-03-15 15:55:50 +0800

    change: move module version to _version

--- src/jinjyaml/__init__.py
+++ src/jinjyaml/__init__.py
@@ -4,8 +4,8 @@ Application specific tag of Jinja2 template in PyYAML.
 It may be useful if you only want to render special tag nodes in the document, instead of whole YAML string as a template.
 """  # noqa: E501
 
+from ._version import __version__, __version_tuple__
 from .constructor import *  # noqa: F403
 from .data import *  # noqa: F403
 from .functions import *  # noqa: F403
 from .representer import *  # noqa: F403
-from .version import __version__, __version_tuple__
